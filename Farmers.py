f258347cc36a4faa7a33e18554bdc1f19c58b39e
Konstantin Kuzmin <kmkuzmin@gmail.com>
2022-12-06 13:50:43 -0500

    Initial version of Farmers Market.

---
+++ Farmers.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  6 12:45:01 2022
+
+@author: konst
+"""
+
+farmers = {}
+
+# with open('Export.csv', 'r') as f:
+#     for data in f:
+#         data_row = data.strip().split(',')
+#         #print(data_row)
+#         farmers[data_row[0]] = data_row[1:]
+#print(farmers)
+with open('Export.csv', 'r') as f:
+    for data in f:
+        data_row = data.strip().split(',')
+        #print(data_row)
+        if data_row[11] not in farmers:
+            farmers[data_row[11]] = []
+        farmers[data_row[11]].append(data_row[1:])
+print(farmers)
+
+zip_code = input('Введите почтовый индекс => ')
+for market in farmers[zip_code]:
+    market_proj = [market[0], market[7], market[9], market[19], market[20]]
+    print(" ".join(market_proj))
+
+# city = input('Введите название города => ')
+# state = input('Введите название штата => ')
+
+# for fmid in farmers:
+#     market = farmers[fmid]
+#     market_proj = [market[0], market[7], market[9], market[19], market[20]]
+#     if market_proj[1] == city and market_proj[2] == state:
+#         print(" ".join(market_proj))
+
+    
