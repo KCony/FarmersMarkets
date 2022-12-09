881b26a5d244e74e2ddff100fe59fad0a1d39027
nekituall <nekituall@gmail.com>
2022-12-09 21:48:01 +0300

    заготовка тест

---
+++ test.py
@@ -0,0 +1,19 @@
+
+
+# import model
+
+testcases_input = []
+testcases_result = []
+
+count = 0
+passed = 0
+failed = 0
+
+
+for test in testcases_input:
+    res = model.find(test)
+    if res == testcases_result[count]:
+        passed +=1 # счетчик выполненных тестов
+    else:
+        failed +=1  # счетчик проваленных тестов
+    count += 1  #общий счетчик тестов
