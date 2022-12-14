f1d9df4c9642372bf6489c4a767727985919839e
Alexander Peredelskikh <apmedic@yandex.ru>
2022-12-14 17:16:54 +0300

    little update commands.

--- console_ui.py
+++ console_ui.py
@@ -4,8 +4,12 @@ def print_ui(cmd, res):
         print('The list of market names')
         for val in res:
             print(val)
-    if cmd == 'list cities':
+    elif cmd == 'list cities':
         print('The list of all cities')
+        for val in res:
+            print(val)
+    elif cmd == 'list cities ASC':
+        print('The list of cities in alphabetical order')
         for val in res:
             print(val)
 def get_command_prompt():
