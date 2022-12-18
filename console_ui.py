652de7c64d447013365e747daab5b3a37dcd5f3e
fletch <fletch4503@gmail.com>
2022-12-18 23:58:53 +0300

    console_controller.py -> add find to process_command console_ui.py -> changed prints error, list and find commands

--- console_ui.py
+++ console_ui.py
@@ -1,17 +1,17 @@
 def print_ui(cmd, res):
-    print(f'Your requested command was {cmd}')
-    if (type(cmd) is list) and (len(cmd) > 1):  # Если передается список и команда требует дополнительных аргументов
+    # print(f'Your requested command was {cmd}')
+    if cmd == 'error':
+        print("List of available commands: ", res)  # Если передается команда, не входящая в список доступных
+    if (cmd is not None) and len(cmd) == 1:  # Если это команда list без аргументов
      if cmd[0] == 'list':   # Если передали команду list
         print('The list of market names')
         for val in res: print(val)
-    # elif (type(cmd) is list) and (cmd[0] == 'list'):
-    #     print('The list of market names')
-    #     for val in res:
-    #         print(val)
-    else: print("List of available commands: ", res)  # Если передается команда, не входящая в список доступных
+    if (len(cmd) > 1) and (cmd[0] == 'find'):  # find передается список и команда требует дополнительных аргументов
+        print("Command: ", cmd)
+
 
 def get_command_prompt():
     return 'Input your command => '
 
 def get_arguments_prompt(command):
-    return 'Input additional arguments for command', command, '=> '
+    return 'Input additional arguments for command ' + command + ': '
