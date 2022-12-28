79ebdfa20d50c5b085750b8294fc2849bb87f027
Roman <Raven9880@mail.ru>
2022-12-28 22:36:27 +0300

    added tables, viewing command, adding comments and ratings for markets

--- console_ui.py
+++ console_ui.py
@@ -1,7 +1,13 @@
+"""
+Console UI
+"""
+
+
 def print_ui(cmd, res):
+    """docstring"""
     print(f'Your requested command was {cmd}')
-    if (type(cmd) is list) and (len(cmd) > 1):  # Если передается список и команда требует дополнительных аргументов
-        if cmd[0] == 'list':   # Если передали команду list
+    if (type(cmd) is list) and (len(cmd) > 1):
+        if cmd[0] == 'list':
             print('The list of market names')
             for val in res:
                 print(val)
@@ -10,12 +16,14 @@ def print_ui(cmd, res):
     #     for val in res:
     #         print(val)
     else:
-        print("List of available commands: ", res)  # Если передается команда, не входящая в список доступных
+        print("List of available commands: ", res)
 
 
 def get_command_prompt():
+    """pass"""
     return 'Input your command => '
 
 
 def get_arguments_prompt(command):
+    """pass"""
     return 'Input additional arguments for command ' + command + ' => '
