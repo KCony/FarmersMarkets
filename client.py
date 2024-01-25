bfab8b4dd4c4baee7f695e4e0e22aa0846b143a8
Dmitrii.Korsunov <dmitrii.korsunov@atom.team>
2024-01-25 21:49:18 +0300

    Added new method in Viewer class

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ class ShowMainMenu:
 
     def __init__(self):
         self.show_list = []
+        self.show_market_spisok = {}
         self.database = s.Database()
         if not self.database.open_database("Export.csv"):
             pass
@@ -26,14 +27,19 @@ class ShowMainMenu:
         print(*self.menu.values(), sep="\n")
 
     def main_menu(self, cmd):
+
         if cmd == "1":
-            print("pass")
+            sheet_number = int(input("Введите номер страницы: "))
+            self.show_market_spisok = self.database.show_sheet(sheet_number)
+            viewer = Viewer(self, self.show_market_spisok)
+            viewer.show_sheet_1()
+           # print(show_market_spisok)
 
         elif cmd == "2":
             city = input("Введите город: ")
             state = input("Введите штат: ")
             self.show_list = self.database.show_filtered_markets_city_state(city, state)
-            viewer = Viewer(self)
+            viewer = Viewer(self, self.show_list)
             viewer.show_city_state()
         elif cmd == "3":
             print("Empty")
@@ -46,17 +52,24 @@ class ShowMainMenu:
 
 
 class Viewer:
-
-    def __init__(self, showmainmenu):
-        self.show = showmainmenu
+    def __init__(self, showmainmenu, showsheet):
+            self.show = showmainmenu
+            self.sheet = showsheet
 
     def show_city_state(self):
         res_list = []
+
         for i, n in enumerate(self.show.show_list):
             li = dict(n).get("MarketName")
             res_list.append(li)
         print(res_list)
 
+    def show_sheet_1(self):
+        res_spisok = []
+        for i ,n in enumerate(self.sheet):
+            pi = dict(n).get("MarketName")
+            res_spisok.append(pi)
+        print(res_spisok)
 
 if __name__ == '__main__':
     show_main = ShowMainMenu()
